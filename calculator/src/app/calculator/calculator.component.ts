617d22437d5601adc65180670174f8a1e0f5bb17
James Porter <porterjames1993@gmail.com>
2018-09-30 22:13:27 -0700

    working on calculator but has a couple bugs

--- calculator/src/app/calculator/calculator.component.ts
+++ calculator/src/app/calculator/calculator.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  display = "0";
+  memory = [];
+  index = 0;
+  new = true;
+
   constructor() { }
 
   // This is a temporary function that needs to be deleted
@@ -15,6 +20,97 @@ export class CalculatorComponent implements OnInit {
   // to each button in the design.
   // Michael (9/29/18, 8:15pm)
   replaceThis() {}
+
+  result(a) {
+
+    if (this.new == true || this.display == '0' && a == 0 || this.display == '0' && a != 0) {
+      //this.clear();
+      this.display = a;
+    }else {
+      this.display = this.display + "" + a;
+    }
+    this.new = false;
+  }
+
+  op(o) {
+    //console.log(this.memory);
+    if (this.memory[0] == undefined) {
+      if ( this.display != "0") {
+        this.memory[this.index] = this.display + o;
+        this.index++;
+        console.log(this.index);
+      }
+    }else {
+      this.memory[this.index] = this.memory[this.index] + o;
+      this.index++;
+      //this.compute(o);
+    }
+    this.new = true;
+  }
+
+  compute() {
+    console.log("index: " + this.index);
+    console.log("memory: " + this.memory);
+    console.log("display: " + this.display);
+    this.memory[this.index] = this.display;
+    var show = this.display;
+      console.log("here");
+      if (this.memory[0].length > 1) {
+      var o = this.memory[0].charAt(this.memory[0].length-1);
+      this.memory[0] = this.memory[0].substring(0, this.memory[0].length - 1);
+      console.log(o);
+      if (o == "+") {//+
+        var temp = parseInt(this.memory[0]) + parseInt(this.memory[1]);
+        this.display = temp.toString();
+       //return this.display;
+      }
+      if (o == "-") {//-
+        var temp = parseInt(this.memory[0]) - parseInt(this.memory[1]);
+        this.display = temp.toString();
+        //return this.display;
+      }
+      if (o == "x") {//x
+        var temp = parseInt(this.memory[0]) * parseInt(this.memory[1]);
+        this.display = temp.toString();
+        //return this.display;
+      }
+      if (o == "/") {// div
+        var temp = parseInt(this.memory[0]) / parseInt(this.memory[1]);
+        this.display = temp.toString();
+       // return this.display; 
+      }
+      this.index = 0;
+      console.log("memory: " + this.memory);
+      console.log("display: " + this.display);
+      this.memory[1] = this.display;
+      console.log("memory: " + this.memory);
+      this.memory.splice(0,1);
+      console.log("index: " + this.index);
+      console.log("memory: " + this.memory);
+      this.new = true;
+
+    }else { 
+      console.log("clearrrrrrr");
+      this.clear();
+      this.display = show;
+    }
+  }
+
+  clear() {
+    this.display = '0';
+    this.index = 0;
+    this.memory = [];
+    this.new = true;
+    console.log(this.memory);
+  }
+
+  delete() {
+    if (this.display != '0' && this.display.length > 1) {
+    this.display = this.display.substring(0, this.display.length-1);
+    }else {
+      this.clear();
+    }
+  }
   
 
   ngOnInit() {
